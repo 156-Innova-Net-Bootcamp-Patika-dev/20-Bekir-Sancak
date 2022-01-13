aa37e790c8d8cdea60ba0ccab96471e5392c908e
Bekir Sancak <37635378+BekirSancak09@users.noreply.github.com>
2022-01-13 04:37:44 +0300

    Add files via upload

---
+++ MyWebApi/Controllers/BooksController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> _books;
+        public BooksController()
+        {
+            _books = new List<Book> {
+                new Book {BookId=1,BookName="Düşüncenin Gücü", Writer="James Allen",Publisher="İkilem Yayınevi",PublicationYear=2019},
+                new Book {BookId=2,BookName="IFA-Beden", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
+                new Book {BookId=3,BookName="IFA-İlişkiler ve Stres", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
+                new Book {BookId=4,BookName="IFA-Sınırları Aşmak", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
+                new Book {BookId=5,BookName="Bitir", Writer="John Acuff",Publisher="Pasaj",PublicationYear=2019},
+                new Book {BookId=6,BookName="Ataleti Yenmek", Writer="Mumin Sekman",Publisher="ALFA",PublicationYear=2020}
+            };
+        }
+
+        /// <summary>
+        /// Kitap listesi getirir
+        /// </summary>
+        /// <returns></returns>
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _books.ToList();
+            return Ok(result);
+        }
+        /// <summary>
+        /// Parametre olarak verilen id'ye göre tek bir kitap getirir
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+
+        [HttpGet("getById")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            var result = _books.FirstOrDefault(c => c.BookId == id);
+            if (result == null)
+            {
+                return BadRequest("Kitap bulunamadı");
+            }
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Kitap listesine bir kitap ekler
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] Book book)
+        {
+            _books.Add(book);
+
+            return Ok("Kitap Eklendi");
+        }
+
+        /// <summary>
+        /// Parametre olarak verilen book nesnesine göre günceller.
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] Book book)
+        {
+            var result = _books.FirstOrDefault(c => c.BookId == book.BookId);
+            if (result == null)
+            {
+                return BadRequest("Kitap güncellenemedi");
+            }
+            result.BookName = book.BookName;
+            result.Writer = book.Writer;
+            result.Publisher = book.Publisher;
+            result.PublicationYear = book.PublicationYear;
+
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Parametre olarak verilen book nesnesinin BookId'sine göre siler.
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromBody] Book book)
+        {
+            Book result = _books.FirstOrDefault(c => c.BookId == book.BookId);
+            if (result == null)
+            {
+                return BadRequest("Kitap silinemedi");
+            }
+            _books.Remove(result);
+
+
+            return Ok(result);
+        }
+    }
+}
+
---
+++ MyWebApi/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Entities
+{
+    public class Book
+    {
+       
+        public int BookId { get; set; }
+
+        public string BookName { get; set; }
+        public string Writer { get; set; }
+        public string Publisher { get; set; }
+        public int PublicationYear { get; set; }
+    }
+}
