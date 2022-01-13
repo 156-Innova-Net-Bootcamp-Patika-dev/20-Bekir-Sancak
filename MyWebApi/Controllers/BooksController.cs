6125c18d60be5b98cdf89fb1206c051329a0ab23
Bekir Sancak <37635378+BekirSancak09@users.noreply.github.com>
2022-01-14 00:14:59 +0300

    Update BooksController.cs

--- MyWebApi/Controllers/BooksController.cs
+++ MyWebApi/Controllers/BooksController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyWebApi.Entities;
 using System;
@@ -16,12 +16,12 @@ namespace MyWebApi.Controllers
         public BooksController()
         {
             _books = new List<Book> {
-                new Book {BookId=1,BookName="Düşüncenin Gücü", Writer="James Allen",Publisher="İkilem Yayınevi",PublicationYear=2019},
-                new Book {BookId=2,BookName="IFA-Beden", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
-                new Book {BookId=3,BookName="IFA-İlişkiler ve Stres", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
-                new Book {BookId=4,BookName="IFA-Sınırları Aşmak", Writer="Sinan Canan",Publisher="Tuti Kitap",PublicationYear=2020},
-                new Book {BookId=5,BookName="Bitir", Writer="John Acuff",Publisher="Pasaj",PublicationYear=2019},
-                new Book {BookId=6,BookName="Ataleti Yenmek", Writer="Mumin Sekman",Publisher="ALFA",PublicationYear=2020}
+                new Book {BookId=1,BookName="Düşüncenin Gücü", Author="James Allen",Publisher="İkilem Yayınevi",PublishDate=2019},
+                new Book {BookId=2,BookName="IFA-Beden", Author="Sinan Canan",Publisher="Tuti Kitap",PublishDate=2020},
+                new Book {BookId=3,BookName="IFA-İlişkiler ve Stres", Author="Sinan Canan",Publisher="Tuti Kitap",PublishDate=2020},
+                new Book {BookId=4,BookName="IFA-Sınırları Aşmak", Author="Sinan Canan",Publisher="Tuti Kitap",PublishDate=2020},
+                new Book {BookId=5,BookName="Bitir", Author="John Acuff",Publisher="Pasaj",PublishDate=2019},
+                new Book {BookId=6,BookName="Ataleti Yenmek", Author="Mumin Sekman",Publisher="ALFA",PublishDate=2020}
             };
         }
 
@@ -51,21 +51,73 @@ namespace MyWebApi.Controllers
                 return BadRequest("Kitap bulunamadı");
             }
 
-            return Ok("Kitap bulundu");
+            return Ok(result);
         }
+
+        /// <summary>
+        /// Kitap adına göre arama yapıldığında listeden kitabı getirir.
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns></returns>
+        [HttpGet("searchBook")]
+        public IActionResult GetBook([FromQuery] string book)
+        {
+            if (!string.IsNullOrEmpty(book))
+            {
+                var result = _books.Where(b => b.BookName.ToLower().Contains(book.ToLower()));
+                return Ok(result);
+            }
+
+            return BadRequest("Kitap bulunamadı");
+        }
+
+        /// <summary>
+        /// Yazara göre arama yapıldığında listeden yazara ait kitapları getirir
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <returns></returns>
+        [HttpGet("searchAuthor")]
+        public IActionResult SearchWriter([FromQuery] string writer)
+        {
+           if(!string.IsNullOrEmpty(writer))
+            {
+              var result=  _books.Where(b => b.Author.ToLower().Contains(writer.ToLower())).ToList();
+                return Ok(result);
+            }
+
+            return BadRequest("Yazar bulunamadı");
+        }
+
+        /// <summary>
+        /// Yayın evine göre arama yapıldığında listeden yayın evine ait kitapları getirir
+        /// </summary>
+        /// <param name="publisher"></param>
+        /// <returns></returns>
+        [HttpGet("searchPublisher")]
+        public IActionResult SearchPublisher([FromQuery] string publisher)
+        {
+            if (!string.IsNullOrEmpty(publisher))
+            {
+                var result = _books.Where(b => b.Publisher.ToLower().Contains(publisher.ToLower())).ToList();
+                return Ok(result);
+            }
+
+            return BadRequest("Yayın evi bulunamadı");
+        }
+
 
         /// <summary>
         /// Kitap listesine bir kitap ekler
         /// </summary>
         /// <param name="book"></param>
         /// <returns></returns>
-        
+
         [HttpPost("add")]
         public IActionResult Add([FromBody] Book book)
         {
             _books.Add(book);
 
-            return Ok("Kitap Eklendi");
+            return Ok();
         }
 
         /// <summary>
@@ -83,9 +135,9 @@ namespace MyWebApi.Controllers
                 return BadRequest("Kitap güncellenemedi");
             }
             result.BookName = book.BookName;
-            result.Writer = book.Writer;
+            result.Author = book.Author;
             result.Publisher = book.Publisher;
-            result.PublicationYear = book.PublicationYear;
+            result.PublishDate = book.PublishDate;
 
 
             return Ok("Kitap güncellendi");
@@ -112,4 +164,3 @@ namespace MyWebApi.Controllers
         }
     }
 }
-
